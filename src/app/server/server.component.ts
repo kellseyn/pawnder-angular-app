1940927bbf326c7229ebd2eaf29ee2fee40aaac0
Kellsey Neagley <kellseyneagley@gmail.com>
2020-03-18 14:54:06 -0500

    learned to apply string interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,5 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './server.component.html'
 })
 export class ServerComponent {
-
+  serverId: number = 10;
+  serverStatus: string = 'offline';
 }
+
