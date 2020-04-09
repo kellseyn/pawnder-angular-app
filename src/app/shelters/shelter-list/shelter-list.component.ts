f038321073b5b58ea5e461e3952f7c1361fb3d64
Kellsey Neagley <kellseyneagley@gmail.com>
2020-04-09 14:08:40 -0500

    added content for shelter component

--- src/app/shelters/shelter-list/shelter-list.component.ts
+++ src/app/shelters/shelter-list/shelter-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {  Shelter } from '../shelter.model';
 
 @Component({
   selector: 'app-shelter-list',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shelter-list.component.css']
 })
 export class ShelterListComponent implements OnInit {
-  shelters = [];
+  shelters: Shelter[] = [
+    new Shelter('Coit North Vet Hospital',
+      'Plano, Tx',
+      '(214) 618-8282',
+      'http://s3.amazonaws.com/assets.brightspot.vetstreet.com/assets/ac/15/c6b1aed2-ef80-438d-9c6e-7fef6b81c8f4.jpg')
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/shelters/shelter.model.ts
+++ src/app/shelters/shelter.model.ts
@@ -4,4 +4,11 @@ export class Shelter {
   public phoneNumber: string;
   public imagePath: string;
 
+  constructor(name: string, location: string, phoneNumber: string, imagePath: string) {
+    this.name = name;
+    this.location = location;
+    this.phoneNumber = phoneNumber;
+    this.imagePath = imagePath;
+  }
+
 }
