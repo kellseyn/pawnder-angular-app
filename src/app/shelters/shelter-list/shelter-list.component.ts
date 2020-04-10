4e6c7414c8af1f590a1c1709d8aca4cb9045e811
Kellsey Neagley <kellseyneagley@gmail.com>
2020-04-10 16:38:44 -0500

    displayed shelter in detail component

--- src/app/shelters/shelter-list/shelter-list.component.ts
+++ src/app/shelters/shelter-list/shelter-list.component.ts
@@ -8,7 +8,7 @@ import {  Shelter } from '../shelter.model';
 })
 export class ShelterListComponent implements OnInit {
   shelters: Shelter[] = [
-    new Shelter('Coit North Vet Hospital',
+    new Shelter('Coit North Veterinary Hospital',
       'Plano, Tx',
       '(214) 618-8282',
       'http://s3.amazonaws.com/assets.brightspot.vetstreet.com/assets/ac/15/c6b1aed2-ef80-438d-9c6e-7fef6b81c8f4.jpg')
