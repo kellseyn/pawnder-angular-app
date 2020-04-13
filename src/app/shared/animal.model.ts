8b4a2b9b1912209f820b5f698fece66a35657c67
Kellsey Neagley <kellseyneagley@gmail.com>
2020-04-13 16:05:10 -0500

    added animal model

---
+++ src/app/shared/animal.model.ts
@@ -0,0 +1,14 @@
+export class Animal {
+  public name: string;
+  public gender: string;
+  public age: number;
+  public bio: string;
+
+  constructor(name: string, gender: string, age: number, bio: string) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.bio = bio;
+  }
+
+}
