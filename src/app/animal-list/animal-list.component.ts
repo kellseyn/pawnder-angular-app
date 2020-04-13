f89564d97f7290d866ba612f16d96fe59440e514
Kellsey Neagley <kellseyneagley@gmail.com>
2020-04-13 16:17:56 -0500

    created and outputted the animal list

--- src/app/animal-list/animal-list.component.ts
+++ src/app/animal-list/animal-list.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Animal } from '../shared/animal.model';
+
 @Component({
   selector: 'app-animal-list',
   templateUrl: './animal-list.component.html',
   styleUrls: ['./animal-list.component.css']
 })
 export class AnimalListComponent implements OnInit {
-  animals = [];
+  animals: Animal[] = [
+    new Animal('Houdini', 'M', 2 , 'He is neutered but likes to escape'),
+    new Animal('Sassy', 'F', 4, 'Just like her name says, she is very sassy')
 
+  ];
   constructor() { }
 
   ngOnInit(): void {
--- src/app/shared/animal.model.ts
+++ src/app/shared/animal.model.ts
@@ -1,14 +1,4 @@
 export class Animal {
-  public name: string;
-  public gender: string;
-  public age: number;
-  public bio: string;
-
-  constructor(name: string, gender: string, age: number, bio: string) {
-    this.name = name;
-    this.gender = gender;
-    this.age = age;
-    this.bio = bio;
-  }
+  constructor(public name: string, public gender: string, public age: number, public bio: string) {}
 
 }
