74b71d72780d3909dbf9f0a1bd2102e08095946d
Kellsey Neagley <kellseyneagley@gmail.com>
2020-04-15 16:28:56 -0500

    passing shelter data using databinding

--- src/app/shelters/shelter-list/shelter-name/shelter-name.component.ts
+++ src/app/shelters/shelter-list/shelter-name/shelter-name.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {Shelter} from '../../shelter.model';
+
 
 @Component({
   selector: 'app-shelter-name',
@@ -6,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shelter-name.component.css']
 })
 export class ShelterNameComponent implements OnInit {
+  @Input() shelter: Shelter;
 
   constructor() { }
 
