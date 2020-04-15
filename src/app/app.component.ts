1af208376a23a92531530a2551429ff940402401
Kellsey Neagley <kellseyneagley@gmail.com>
2020-04-15 14:57:53 -0500

    added navigation with event binding and ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,9 +15,14 @@ export class AppComponent {
   username = '';
   showSecret = false;
   log = [];
+  loadedFeature = 'shelter';
+
   onToggleDetails() {
     this.showSecret = !this.showSecret;
     // this.log.push(this.log.length + 1);
     this.log.push(new Date());
+  }
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
   }
 }
