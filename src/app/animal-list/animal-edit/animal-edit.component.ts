14e088ef99131744e15ad8f599c72786142ee67d
Kellsey Neagley <kellseyneagley@gmail.com>
2020-04-16 16:50:49 -0500

    the  user can add an animal to the animal list and see an updated list of animals inthe shelter

--- src/app/animal-list/animal-edit/animal-edit.component.ts
+++ src/app/animal-list/animal-edit/animal-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, ViewChild, ElementRef, EventEmitter } from '@angular/core';
+import {Animal} from '../../shared/animal.model';
 
 @Component({
   selector: 'app-animal-edit',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./animal-edit.component.css']
 })
 export class AnimalEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('ageInput') ageInputRef: ElementRef;
+  @ViewChild('genderInput') genderInputRef: ElementRef;
+  @ViewChild('bioInput') bioInputRef: ElementRef;
+  @ViewChild('imgPathInput') imgPathInputRef: ElementRef;
+  @Output() animalAdded = new EventEmitter<Animal>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onAddAnimal() {
+    const animalName = this.nameInputRef.nativeElement.value;
+    const animalAge = this.ageInputRef.nativeElement.value;
+    const animalGender = this.genderInputRef.nativeElement.value;
+    const animalBio = this.bioInputRef.nativeElement.value;
+    const animalImgPath = this.imgPathInputRef.nativeElement.value;
+    const newAnimal = new Animal(animalName, animalGender, animalAge, animalImgPath, animalBio);
+    this.animalAdded.emit(newAnimal);
   }
 
 }
--- src/app/animal-list/animal-list.component.ts
+++ src/app/animal-list/animal-list.component.ts
@@ -11,11 +11,13 @@ export class AnimalListComponent implements OnInit {
   animals: Animal[] = [
     new Animal('Houdini', 'M', 2 , 'https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/46410177/1/?bust=1572306753&width=1080' , 'He is neutered but likes to escape'),
     new Animal('Sassy', 'F', 4, 'https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/47616527/1/?bust=1584192336&width=1080' , 'Just like her name says, she is very sassy')
-
   ];
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onAnimalAdded(animal: Animal) {
+    this.animals.push(animal);
+  }
 }
