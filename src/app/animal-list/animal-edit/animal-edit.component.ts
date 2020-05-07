469944fa5d1e48c0e12abd338cf7a337f18f4118
Kellsey Neagley <kellseyneagley@gmail.com>
2020-05-07 16:34:15 -0500

    refactoring my form to be template driven

--- src/app/animal-list/animal-edit/animal-edit.component.ts
+++ src/app/animal-list/animal-edit/animal-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, ViewChild, ElementRef, EventEmitter } from '@angular/core';
 import {Animal} from '../../shared/animal.model';
 import {AnimalListService} from '../animal-list.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-animal-edit',
@@ -8,24 +9,25 @@ import {AnimalListService} from '../animal-list.service';
   styleUrls: ['./animal-edit.component.css']
 })
 export class AnimalEditComponent implements OnInit {
-  @ViewChild('nameInput') nameInputRef: ElementRef;
-  @ViewChild('ageInput') ageInputRef: ElementRef;
-  @ViewChild('genderInput') genderInputRef: ElementRef;
-  @ViewChild('bioInput') bioInputRef: ElementRef;
-  @ViewChild('imgPathInput') imgPathInputRef: ElementRef;
+  // @ViewChild('nameInput') nameInputRef: ElementRef;
+  // @ViewChild('ageInput') ageInputRef: ElementRef;
+  // @ViewChild('genderInput') genderInputRef: ElementRef;
+  // @ViewChild('bioInput') bioInputRef: ElementRef;
+  // @ViewChild('imgPathInput') imgPathInputRef: ElementRef;
 
   constructor(private alService: AnimalListService) { }
 
   ngOnInit(): void {
   }
 
-  onAddAnimal() {
-    const animalName = this.nameInputRef.nativeElement.value;
-    const animalAge = this.ageInputRef.nativeElement.value;
-    const animalGender = this.genderInputRef.nativeElement.value;
-    const animalBio = this.bioInputRef.nativeElement.value;
-    const animalImgPath = this.imgPathInputRef.nativeElement.value;
-    const newAnimal = new Animal(animalName, animalGender, animalAge, animalImgPath, animalBio);
+  onAddAnimal(form: NgForm) {
+    const value = form.value;
+    // const animalName = this.nameInputRef.nativeElement.value;
+    // const animalAge = this.ageInputRef.nativeElement.value;
+    // const animalGender = this.genderInputRef.nativeElement.value;
+    // const animalBio = this.bioInputRef.nativeElement.value;
+    // const animalImgPath = this.imgPathInputRef.nativeElement.value;
+    const newAnimal = new Animal(value.name, value.gender, value.age, value.imgURL, value.bio);
     this.alService.addAnimal(newAnimal);
   }
 
