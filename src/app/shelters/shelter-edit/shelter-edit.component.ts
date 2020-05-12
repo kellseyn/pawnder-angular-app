96e84e776ef27ce8d81035b042f2797e24880316
Kellsey Neagley <kellseyneagley@gmail.com>
2020-05-12 15:20:02 -0500

    created form for editing Shelters

--- src/app/shelters/shelter-edit/shelter-edit.component.ts
+++ src/app/shelters/shelter-edit/shelter-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+import { ShelterService } from '../shelter.service';
 
 @Component({
   selector: 'app-shelter-edit',
@@ -9,9 +11,10 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class ShelterEditComponent implements OnInit {
   id: number;
   editMode = false;
+  shelterForm: FormGroup;
 
-
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private shelterService: ShelterService) { }
 
   ngOnInit(){
     this.route.params
@@ -19,8 +22,30 @@ export class ShelterEditComponent implements OnInit {
         (params: Params) => {
           this.id = +params['id'];
           this.editMode = params['id'] != null;
+          this.initForm();
         }
-      )
+      );
   }
 
+  private initForm() {
+    let shelterName = '';
+    let shelterImagePath = '';
+    let shelterLocation = '';
+    let shelterPhoneNumber = '';
+
+
+    if (this.editMode) {
+      const shelter = this.shelterService.getShelter(this.id);
+      shelterName = shelter.name;
+      shelterImagePath = shelter.imagePath;
+      shelterLocation = shelter.location;
+      shelterPhoneNumber = shelter.phoneNumber;
+    }
+    this.shelterForm = new FormGroup({
+      'name': new FormControl(shelterName),
+      'imagePath': new FormControl(shelterImagePath),
+      'location': new FormControl(shelterLocation),
+      'phoneNumber': new FormControl(shelterPhoneNumber)
+    });
+  }
 }
