87cbdec772cd3304b5a7e9da42d26ce421b3fcd6
Kellsey Neagley <kellseyneagley@gmail.com>
2020-05-20 16:55:56 -0500

    finished the http request lecture

--- src/app/shelters/shelters-resolver.service.ts
+++ src/app/shelters/shelters-resolver.service.ts
@@ -2,13 +2,20 @@ import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Shelter } from './shelter.model';
 import { DataStorageService } from '../shared/data-storage.service';
+import { ShelterService } from './shelter.service';
 
 
 @Injectable({providedIn: 'root'})
 export class ShelterResolverService implements Resolve<Shelter[]> {
-    constructor(private dataStorageService: DataStorageService) {}
+    constructor(private dataStorageService: DataStorageService, private shelterService: ShelterService) {}
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        return this.dataStorageService.fetchShelters();
+        const shelters = this.shelterService.getShelters();
+        if (shelters.length === 0) {
+            return this.dataStorageService.fetchShelters();
+
+        } else {
+            return shelters;
+        }
     }
 }
