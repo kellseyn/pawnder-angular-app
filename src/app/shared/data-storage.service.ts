b5d3b9a711ae3163ff13180bfc63b7badf3feeee
Kellsey Neagley <kellseyneagley@gmail.com>
2020-05-20 15:25:00 -0500

    set up save data for shelters

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ShelterService } from '../shelters/shelter.service';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient, private shelterService: ShelterService) {}
+
+    storeShelters() {
+        const shelters = this.shelterService.getShelters();
+        return this.http.put('https://pawnder-angular-app.firebaseio.com/shelters.json', 
+        shelters
+        )
+        .subscribe(response => {
+            console.log(response);
+        });
+    }
 }
