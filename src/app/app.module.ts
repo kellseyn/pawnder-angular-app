4b5391666c155d0d7e4c3e78e57d5fea865fd5eb
Kellsey Neagley <kellseyneagley@gmail.com>
2020-06-16 14:13:37 -0500

    added the placeholder directive to the app module as an import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,7 @@ import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service'
 import { AlertComponent } from './shared/alert/alert.component';
+import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
 
 
 
@@ -48,7 +49,8 @@ import { AlertComponent } from './shared/alert/alert.component';
     ShelterEditComponent,
     AuthComponent,
     LoadingSpinnerComponent,
-    AlertComponent
+    AlertComponent,
+    PlaceholderDirective
   ],
   imports: [
     BrowserModule,
