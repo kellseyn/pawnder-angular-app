63e41c9bc35badbc9f7657f19799b46d48245154
Kellsey Neagley <kellseyneagley@gmail.com>
2020-06-24 13:34:44 -0500

    added core module for services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { HeaderComponent } from './header/header.component';
 import { AnimalListModule } from './animal-list/animal-list.module';
 import { FooterComponent } from './footer/footer.component';
-import { AnimalListService} from './animal-list/animal-list.service';
 import { SplashComponent } from './splash/splash.component';
 import { AppRoutingModule } from './app-routing.module';
-import { ShelterService } from './shelters/shelter.service';
 import { AuthComponent } from './auth/auth.component';
-import { AuthInterceptorService } from './auth/auth-interceptor.service'
 import { SheltersModule } from './shelters/shelters.module';
 import { SharedModule } from './shared/shared.module';
-
-
+import { CoreModule } from './core.module';
 
 @NgModule({
   declarations: [
@@ -38,17 +33,10 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     SheltersModule,
     AnimalListModule,
-    SharedModule
+    SharedModule,
+    CoreModule
   ],
-  providers: [
-    AnimalListService, 
-    ShelterService, 
-    {
-      provide: HTTP_INTERCEPTORS, 
-      useClass: AuthInterceptorService, 
-      multi: true
-    }
-  ],
+  
   bootstrap: [AppComponent]
   
 })
