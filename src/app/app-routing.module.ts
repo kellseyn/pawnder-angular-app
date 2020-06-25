40e746627528eaa4638522cbfbef256da2377b66
Kellsey Neagley <kellseyneagley@gmail.com>
2020-06-25 15:47:12 -0500

    doing lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {AnimalListComponent} from './animal-list/animal-list.component';
 import {SplashComponent} from './splash/splash.component';
 
-import { AuthComponent } from './auth/auth.component';
 
 const appRoutes: Routes = [
-    { path: '', component: SplashComponent }
+    { path: '', component: SplashComponent },
+    { path: 'shelters', loadChildren: './shelters/shelters.module.ts' }
   ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
@@ -10,7 +9,6 @@ import { AnimalListModule } from './animal-list/animal-list.module';
 import { FooterComponent } from './footer/footer.component';
 import { SplashComponent } from './splash/splash.component';
 import { AppRoutingModule } from './app-routing.module';
-import { AuthComponent } from './auth/auth.component';
 import { SheltersModule } from './shelters/shelters.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
