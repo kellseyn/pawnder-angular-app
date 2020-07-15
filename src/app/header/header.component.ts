5de21b10da985fdedde35d272a02c6793195701d
Kellsey Neagley <kellseyneagley@gmail.com>
2020-07-15 16:07:29 -0500

    finished auth ... for now

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
 import { AuthService } from '../auth/auth.service';
 import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../store/app.reducer';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -14,12 +17,16 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   constructor(
     private dataStorageService: DataStorageService, 
-    private authService: AuthService
+    private authService: AuthService,
+    private store: Store<fromApp.AppState>
     ) { }
 
 
   ngOnInit(){
-    this.userSub = this.authService.user.subscribe(user => {
+    this.userSub = this.store
+    .select('auth')
+    .pipe(map(authState => authState.user))
+    .subscribe(user => {
       this.isAuthenticated = !!user;
       console.log(!user);
       console.log(!!user);
@@ -29,16 +36,16 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   onSaveData() {
     this.dataStorageService.storeShelters();
-    this.dataStorageService.storeAnimals();
+    // this.dataStorageService.storeAnimals();
   }
 
   onFetchShelters() {
     this.dataStorageService.fetchShelters().subscribe();
   }
 
-  onFetchAnimals() {
-        this.dataStorageService.fetchAnimals().subscribe();
-  }
+  // onFetchAnimals() {
+  //       this.dataStorageService.fetchAnimals().subscribe();
+  // }
 
   onLogout() {
     this.authService.logout();
