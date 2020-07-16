c207f404dbf2755ae89ef176a6c8fbfe0d5ede52
Kellsey Neagley <kellseyneagley@gmail.com>
2020-07-16 17:25:39 -0500

    moved methods from service to effects

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -22,6 +22,7 @@ export class AuthComponent implements OnInit, OnDestroy{
     @ViewChild(PlaceholderDirective) alerthost: PlaceholderDirective;
 
     private closeSub: Subscription;
+    private storeSub: Subscription;
 
     constructor(
         private authService: AuthService, 
@@ -32,7 +33,7 @@ export class AuthComponent implements OnInit, OnDestroy{
         ) {}
 
     ngOnInit(){
-        this.store.select('auth').subscribe(authState => {
+        this.storeSub = this.store.select('auth').subscribe(authState => {
             this.isLoading = authState.loading;
             this.error = authState.authError;
             if (this.error) {
@@ -67,12 +68,15 @@ export class AuthComponent implements OnInit, OnDestroy{
     }
 
     onHandleError(){
-        this.error = null;
+        this.store.dispatch(new AuthActions.ClearError());
     }
 
     ngOnDestroy(){
         if (this.closeSub){
             this.closeSub.unsubscribe();
+        }
+        if (this.storeSub) {
+            this.storeSub.unsubscribe();
         }
     }
 
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -30,49 +30,6 @@ export class AuthService {
         private router: Router, 
         private store: Store<fromApp.AppState>){}
 
-        signup(email: string, password: string) {
-            return this.http.post<AuthResponseData>(
-                'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + environment.firebaseAPIKey,
-            {
-                email: email,
-                password: password,
-                returnSecureToken: true
-
-            })
-            .pipe(catchError(this.handleError), 
-                tap(resData => {
-                    this.handleAuthentication(
-                        resData.email, 
-                        resData.localId, 
-                        resData.idToken, 
-                        +resData.expiresIn
-                        );
-                })
-            );
-        }
-
-    login(email: string, password: string) {
-        return this.http.post<AuthResponseData>(
-            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + environment.firebaseAPIKey, 
-            {
-                email: email,
-                password: password,
-                returnSecureToken: true
-
-            }
-        )
-        .pipe(catchError(this.handleError),
-            tap(resData => {
-                this.handleAuthentication(
-                    resData.email, 
-                    resData.localId, 
-                    resData.idToken, 
-                    +resData.expiresIn
-                    );
-            })
-        );
-    }
-
     autoLogin() {
         const userData: {
             email: string;
@@ -108,7 +65,6 @@ export class AuthService {
     logout() {
         // this.user.next(null);
         this.store.dispatch(new AuthActions.Logout());
-        this.router.navigate(['./']);
         localStorage.removeItem('userData');
         if (this.tokenExpirationTimer) {
             clearTimeout(this.tokenExpirationTimer);
