ba1e3d750166b029f4fc9f408efc6054d0c0dc36
Kellsey Neagley <kellseyneagley@gmail.com>
2020-07-16 15:37:45 -0500

    managed the UI state in ngrx

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,4 @@
-import { Component, ComponentFactoryResolver, ViewChild, OnDestroy} from '@angular/core';
+import { Component, ComponentFactoryResolver, ViewChild, OnDestroy, OnInit} from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService, AuthResponseData } from './auth.service';
 import { Observable, Subscription } from 'rxjs';
@@ -15,7 +15,7 @@ import * as AuthActions from './store/auth.actions';
     selector: 'app-auth',
     templateUrl: './auth.component.html'
 })
-export class AuthComponent implements OnDestroy{
+export class AuthComponent implements OnInit, OnDestroy{
     isLoginMode = true;
     isLoading = false;
     error: string = null;
@@ -30,6 +30,13 @@ export class AuthComponent implements OnDestroy{
         // private dataStorageService: DataStorageService,
         private store: Store<fromApp.AppState>
         ) {}
+
+    ngOnInit(){
+        this.store.select('auth').subscribe(authState => {
+            this.isLoading = authState.loading;
+            this.error = authState.authError;
+        });
+    }
 
     onSwitchMode() {
         this.isLoginMode = !this.isLoginMode;
@@ -56,19 +63,21 @@ export class AuthComponent implements OnDestroy{
             authObs = this.authService.signup(email, password);
         }
 
-        authObs.subscribe(
-            resData => {
-                console.log(resData);
-                this.isLoading = false;
-                this.router.navigate(['./shelters']);
-            },
-            errorMessage => {
-                console.log(errorMessage);
-                this.error = errorMessage;
-                this.showErrorAlert(errorMessage);
-                this.isLoading = false;
-            }
-        );
+    
+
+        // authObs.subscribe(
+        //     resData => {
+        //         console.log(resData);
+        //         this.isLoading = false;
+        //         this.router.navigate(['./shelters']);
+        //     },
+        //     errorMessage => {
+        //         console.log(errorMessage);
+        //         this.error = errorMessage;
+        //         this.showErrorAlert(errorMessage);
+        //         this.isLoading = false;
+        //     }
+        // );
 
         form.reset();
     }
